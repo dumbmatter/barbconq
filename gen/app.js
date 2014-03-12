a8a0cccbd7004a067a3664151c3d1269ba618ced
Jeremy Scheff <jdscheff@gmail.com>
2014-03-12 00:04:09 -0400

    One player warrior surrounded by 200 barbarian warriors, in preparation for working on combat.

--- gen/app.js
+++ gen/app.js
@@ -595,9 +595,10 @@ var game = new Game(1, 40, 80);
 var chromeUI = new ChromeUI();
 var mapUI = new MapUI();
 
-new Units.Warrior(0, [1, 1]);
-new Units.Warrior(0, [20, 40]);
-for (var i = 0; i < 2; i++) {
+for (var i = 0; i < 200; i++) {
+    new Units.Warrior(0, [Math.floor(40 * Math.random()), Math.floor(80 * Math.random())]);
+}
+for (var i = 0; i < 1; i++) {
     new Units.Warrior(1, [Math.floor(40 * Math.random()), Math.floor(80 * Math.random())]);
 }
 
