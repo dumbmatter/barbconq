15d17d66cbee983bafc547a2fada66468fb90126
Jeremy Scheff <jdscheff@gmail.com>
2014-03-12 22:46:50 -0400

    Play around with map size

--- ts/app.ts
+++ ts/app.ts
@@ -6,16 +6,16 @@
 ///<reference path='Game.ts'/>
 ///<reference path='Units.ts'/>
 
-var game = new Game(1, 40, 80);
+var game = new Game(1, 20, 40);
 var chromeUI = new ChromeUI();
 var mapUI = new MapUI();
 var controller = new Controller();
 
 for (var i = 0; i < 200; i++) {
-    new Units.Warrior(0, [Math.floor(40 * Math.random()), Math.floor(80 * Math.random())]);
+    new Units.Warrior(0, [Math.floor(game.map.height * Math.random()), Math.floor(game.map.width * Math.random())]);
 }
 for (var i = 0; i < 1; i++) {
-//    new Units.Warrior(1, [Math.floor(40 * Math.random()), Math.floor(80 * Math.random())]);
+//    new Units.Warrior(1, [Math.floor(game.map.height * Math.random()), Math.floor(game.map.width * Math.random())]);
 }
 
 new Units.Warrior(1, [0, 0]);
