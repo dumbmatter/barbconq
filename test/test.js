75c01ac2fb717e81c69e46295b40e6aa3ae91403
Jeremy Scheff <jdscheff@gmail.com>
2014-04-02 00:17:27 -0400

    Refactor odds test

--- test/test.js
+++ test/test.js
@@ -7,13 +7,67 @@ var assets = {
 var chromeUI = jasmine.createSpyObj("chromeUI", ["eventLog", "onUnitActivated", "showModal"]);
 var mapUI = jasmine.createSpyObj("mapUI", ["render"]);
 
-describe("Combat.Battle.odds()", function() {
-    it("should accurately predict battle outcomes for arbitrary combatants", function() {
-        var attackerRetreats, attackerWins, expectedAttackerRetreats, expectedAttackerWins, i, numFights, odds, params, rand, u1, u2;
+function createBattle(params) {
+    var u1, u2;
 
-        attackerWins = 0;
-        attackerRetreats = 0;
-        numFights = 10000;
+    // New game each time
+    game = new Game(1, 1, 2);
+
+    // Same tiles every time
+    game.map.tiles[0][0].terrain = params.tileTerrain;
+    game.map.tiles[0][0].features = params.tileFeatures;
+    game.map.tiles[0][1].terrain = params.tileTerrain;
+    game.map.tiles[0][1].features = params.tileFeatures;
+
+    // Same units every time
+    u1 = new Units[params.u1Type](config.PLAYER_ID, [0, 0]);
+    u2 = new Units[params.u2Type](config.BARB_ID, [0, 1]);
+    params.u1Promotions.forEach(function (promotion) {
+        u1.promote(promotion, true);
+    });
+    params.u2Promotions.forEach(function (promotion) {
+        u2.promote(promotion, true);
+    });
+    u1.currentStrength *= params.u1HP / 100;
+    u2.currentStrength *= params.u2HP / 100;
+
+    return new Combat.Battle(u1, u2);
+}
+
+function testBattleOdds(params, expectedOddsAttackerWins, expectedOddsAttackerRetreats) {
+    var attackerRetreats, attackerWins, expectedAttackerRetreats, expectedAttackerWins, b, i, numFights;
+
+    numFights = 10000;
+
+    attackerWins = 0;
+    attackerRetreats = 0;
+
+    for (i = 0; i < numFights; i++) {
+        b = createBattle(params);
+        b.fight();
+        if (b.winner === "attacker") {
+            attackerWins += 1;
+        } else if (!b.winner) {
+            attackerRetreats += 1;
+        }
+    }
+
+console.log([expectedOddsAttackerWins, expectedOddsAttackerRetreats])
+    expectedAttackerWins = expectedOddsAttackerWins * numFights;
+    expectedAttackerRetreats = expectedOddsAttackerRetreats * numFights;
+
+console.log("Expected Wins: " + expectedAttackerWins / numFights);
+console.log("Observed Wins: " + attackerWins / numFights);
+console.log("Expected Retreats: " + expectedAttackerRetreats / numFights);
+console.log("Observed Retreats: " + attackerRetreats / numFights);
+
+    expect(Math.abs((attackerWins - expectedAttackerWins) / numFights)).toBeLessThan(0.01);
+    expect(Math.abs((attackerRetreats - expectedAttackerRetreats) / numFights)).toBeLessThan(0.01);
+}
+
+describe("Combat.Battle.odds()", function() {
+    it("should accurately predict outcome for random battle", function() {
+        var b, expectedOddsAttackerRetreats, expectedOddsAttackerWins, i, odds, params, rand;
 
         params = {};
 
@@ -42,56 +96,19 @@ describe("Combat.Battle.odds()", function() {
         params.u1HP = Math.round(Math.random() * 100);
         params.u2HP = Math.round(Math.random() * 100);
 //params = {tileTerrain: "grassland", tileFeatures: ["forest"], u1Type: "Chariot", u2Type: "Archer", u1Promotions: [], u2Promotions: [], u1HP: 100, u2HP: 100};
-//params = {tileTerrain: 'grassland', tileFeatures: [], u1Type: 'Axeman', u2Type: 'Axeman', u1Promotions: ['drill1', 'drill2', 'drill3'], u2Promotions: [], u1HP: 100, u2HP: 100};
+params = {tileTerrain: 'grassland', tileFeatures: [], u1Type: 'Axeman', u2Type: 'Axeman', u1Promotions: ['drill1', 'drill2', 'drill3'], u2Promotions: [], u1HP: 100, u2HP: 100};
 console.log(params);
-        
-        for (i = 0; i < numFights; i++) {
-            game = new Game(1, 1, 2);
 
-            // Same tiles every time
-            game.map.tiles[0][0].terrain = params.tileTerrain;
-            game.map.tiles[0][0].features = params.tileFeatures;
-            game.map.tiles[0][1].terrain = params.tileTerrain;
-            game.map.tiles[0][1].features = params.tileFeatures;
+        b = createBattle(params);
 
-            // Same units every time
-            u1 = new Units[params.u1Type](config.PLAYER_ID, [0, 0]);
-            u2 = new Units[params.u2Type](config.BARB_ID, [0, 1]);
-            params.u1Promotions.forEach(function (promotion) {
-                u1.promote(promotion, true);
-            });
-            params.u2Promotions.forEach(function (promotion) {
-                u2.promote(promotion, true);
-            });
-            u1.currentStrength *= params.u1HP / 100;
-            u2.currentStrength *= params.u2HP / 100;
-
-            b = new Combat.Battle(u1, u2);
-
-            // Save prediction for later
-            if (i === 0) {
-                odds = b.odds();
+        // Calculate odds for battle
+        odds = b.odds();
 console.log(b.appliedBonuses);
 console.log(b.firstStrikes);
 console.log(odds);
-                expectedAttackerWins = odds.attackerWinsFight * numFights;
-                expectedAttackerRetreats = odds.hasOwnProperty("attackerRetreats") ? odds.attackerRetreats * numFights : 0;
-            }
+        expectedOddsAttackerWins = odds.attackerWinsFight;
+        expectedOddsAttackerRetreats = odds.hasOwnProperty("attackerRetreats") ? odds.attackerRetreats : 0;
 
-            b.fight();
-            if (b.winner === "attacker") {
-                attackerWins += 1;
-            } else if (!b.winner) {
-                attackerRetreats += 1
-            }
-        }
-
-console.log("Expected Wins: " + expectedAttackerWins / numFights);
-console.log("Observed Wins: " + attackerWins / numFights);
-console.log("Expected Retreats: " + expectedAttackerRetreats / numFights);
-console.log("Observed Retreats: " + attackerRetreats / numFights);
-
-        expect(Math.abs((attackerWins - expectedAttackerWins) / numFights)).toBeLessThan(0.01);
-        expect(Math.abs((attackerRetreats - expectedAttackerRetreats) / numFights)).toBeLessThan(0.01);
+        testBattleOdds(params, expectedOddsAttackerWins, expectedOddsAttackerRetreats);
     });
 });
