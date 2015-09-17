8640c4b2b802bf5efe165917ef6141c941af00d1
Jeremy Scheff <jdscheff@gmail.com>
2015-09-16 21:34:43 -0400

    More progress on new test format

--- test/test.ts
+++ test/test.ts
@@ -1,14 +1,17 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/chai/chai.d.ts" />
+
+import chai = require('chai');
+var assert = chai.assert;
 
 describe('User Model Unit Tests:', () => {
-
     describe('2 + 4', () => {
-        it('should be 6', (done) => {
-            done();
+        it('should be 6', () => {
+            assert.equal(2+4, 6);
         });
 
-        it('should not be 7', (done) => {
-            done();
+        it('should not be 7', () => {
+            assert.equal(3+4, 7);
         });
     });
 });
