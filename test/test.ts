24ed6a4777f8f2027832df932d8b366dd4a6ea7d
Jeremy Scheff <jdscheff@gmail.com>
2015-09-16 21:26:13 -0400

    Prototype of new test format

---
+++ test/test.ts
@@ -0,0 +1,14 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+
+describe('User Model Unit Tests:', () => {
+
+    describe('2 + 4', () => {
+        it('should be 6', (done) => {
+            done();
+        });
+
+        it('should not be 7', (done) => {
+            done();
+        });
+    });
+});
